b4d52e217ad563783e1cc460a97dfb4413d9b4fa
Henry Lau <henrywlau@yahoo.com>
2017-04-10 15:53:34 -0500

    Initial commit, completed persisting to a single class to add cheese categories.

---
+++ src/main/java/org/launchcode/models/data/CategoryDao.java
@@ -0,0 +1,14 @@
+package org.launchcode.models.data;
+
+import org.launchcode.models.Category;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by henry on 4/6/17.
+ */
+@Repository
+@Transactional
+public interface CategoryDao extends CrudRepository<Category, Integer> {
+}
